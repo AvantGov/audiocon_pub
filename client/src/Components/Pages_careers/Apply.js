2dc73296c74f0f4fd1198dcfce0219d4212dc5ba
avantgov <alexcouts18@gmail.com>
2022-09-02 08:33:45 -0500

    realized job postings were no good so had to update those

--- client/src/Components/Pages_careers/Apply.js
+++ client/src/Components/Pages_careers/Apply.js
@@ -20,10 +20,10 @@ const Apply = () => {
                     <div id="linkedin" className="button__icon"></div>
                 </div>
             </a>
-            <a href="https://www.indeed.com/" className="Apply__link" target="blank" rel="noreferrer noopener">
+            <a href="https://www.facebook.com/jobs/job-opening/775367437113305/?source=post_homepage_stream&__xts__[0]=68.ARB4-8zp_r-NeB3vDv3EuPzvwpxx2V1zF0o_tyrszDa1DkXcoTqMIK9NWeDX8B1dM4oYa0PgebcNM2QasQVyaQD7g7FBZCiAcqiM8kk_JlRTlBV0C7s4C32KKHrlqCDCJigzXs0Oa8gIZc6LJVlUX08_njF0l25nWwMvksPHB2oITUolotj_QW4wagSkU6uvt61cJhuSaCeUGbsycZJf_hiweuSim7UiZ_mRxqJ8Y67_bGbOnXcLq-KHm6RD8qjCq2kS19yuU3z0bTrq5tGu88_mjGHIWwPDoVygEZmRrXeehtzmXVntBSqp-MQJkopUk0qI2w" className="Apply__link" target="blank" rel="noreferrer noopener">
                 <div className="link__button">
-                    <p className="button__lable">Apply on Indeed</p>
-                    <div id="indeed" className="button__icon"></div>
+                    <p className="button__lable">Apply on Facebook</p>
+                    <div id="facebook" className="button__icon"></div>
                 </div>
             </a>
             <Link to="/apply/positions" className="Apply__link" target="blank" rel="noreferrer noopener">
